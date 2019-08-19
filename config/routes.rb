451f196dde549fd3e3b41bb26e74bd6e87edb116
huydx-1752 <dinh.xuan.huy@sun-asterisk.com>
2019-08-19 18:42:18 +0700

    error

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+  root   'static_pages#home'
+  get    '/help',    to: 'static_pages#help'
+  get    '/about',   to: 'static_pages#about'
+  get    '/contact', to: 'static_pages#contact'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
-  get 'static_page/home'
-  get 'static_page/help'
-  get 'static_page/about'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
